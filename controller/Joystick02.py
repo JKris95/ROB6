b77ce2a2b55519d533e0b3607c21333b603edd1b
JKris95 <jk03071995@gmail.com>
2018-04-12 09:07:07 +0200

    Controller commit

--- controller/Joystick02.py
+++ controller/Joystick02.py
@@ -1,18 +1,13 @@
 import time
 import socket
 import RPi.GPIO as GPIO
+
+PORT = 50007
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(36, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Left
 GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Forward
 GPIO.setup(33, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Right
 GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Back
-
-def Connect(HOST, PORT, socket_object):
-        """Connects to the desired turtlebot corresponding to the ip-address
-        passed to it"""
-        socket_object.connect((HOST, PORT))
-
-
 
 def eightWay(s):
     while True:
@@ -111,25 +106,5 @@ def angularWay(s):
         elif GPIO.input(38) == True and GPIO.input(35) == True and GPIO.input(36) == True and GPIO.input(33) == True:
                 print (' ')
                 s.sendall(b' ')
-
-def choseMode():
-        pass
-
-
-if __name__=="__main__":
-    HOST = ''
-    PORT = 50007
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    Connect(HOST, PORT, s)
-    time.sleep(0.5)
-    chose = choseMode()
-    if chose == 1:
-        eightWay(s)
-    elif chose == 2:
-        twoWay(s)
-    elif chose == 3:
-        fourWay(s)
-    elif chose == 4:
-        angularWay(s)
 
 
--- controller/playerClass.py
+++ controller/playerClass.py
@@ -1,14 +1,31 @@
+import socket
+
 class Player():
     
-    self.speed = 1
-    self.control_mode = 'fourway'
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def __init__(self, name = 'player'):
+    def __init__(self, name = 'player', speed = 1, control_mode = 'fourway'):
         self.name = name
-    
+        self.speed = speed
+        self.control_mode = control_mode
 
     def choose_bot38(self):
         self.robot_choice = '192.168.1.38'
     def choose_bot39(self):
         self.robot_choice = '192.168.1.38'
-    
+
+    def Connect(self, HOST, PORT, socket_object):
+        """Connects to the desired turtlebot corresponding to the ip-address
+        passed to it"""
+        try:
+            socket_object.connect((HOST, PORT))
+        except:
+            print("Couldn't connect to TurtleBot with address " + HOST)
+            
+    def drive(self):
+        pass
+
+if __name__ == '__main__':
+    kwargs = {"name": "martin", "speed": 0.5}
+    player1 = Player(**kwargs)
+    print(player1.name, player1.control_mode)
