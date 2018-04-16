a9ce417e19c4ce73b88bd66b4844567ff1c28158
JKris95 <jk03071995@gmail.com>
2018-04-16 17:02:07 +0200

    Send 0 once when joystick is idle | Difficulty updates

--- controller/Joystick02.py
+++ controller/Joystick02.py
@@ -1,110 +1,157 @@
 import time
 import socket
+import json
 import RPi.GPIO as GPIO
+import playerClass
 
+status = {'running':False}
 PORT = 50007
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(36, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Left
-GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Forward
-GPIO.setup(33, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Right
-GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Back
+GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Left
+GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Forward
+GPIO.setup(36, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Right
+GPIO.setup(32, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Back
 
-def eightWay(s):
-    while True:
+
+player = playerClass.Player()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def Connect(self, HOST, PORT, socket_object):
+    """Connects to the desired turtlebot corresponding to the ip-address
+    passed to it"""
+    try:
+        socket_object.connect((HOST, PORT))
+    except:
+        print("Couldn't connect to TurtleBot with address " + HOST) 
+
+def wait_for_input(control_mode):
+	GPIOs = (GPIO.input(40), GPIO.input(38), GPIO.input(36), GPIO.input(32))
+	while True:
+		for GPIO in GPIOs:
+			if not GPIO:
+				return # returns control to drive()
+
+def eight_Way(socket_object):
+    while status['running'] == True:
         time.sleep(0.1)
-        if GPIO.input(38) == False and GPIO.input(33) == True and GPIO.input(36) == True:
-                print ('Forward')
-                s.sendall(b'Forward')
+        if GPIO.input(40) == False and GPIO.input(36) == True and GPIO.input(38) == True:
+            print ('Forward')
+            s.sendall(json.dumps(player.speeds).decode)
 
-        elif GPIO.input(35) == False and GPIO.input(33) == True and GPIO.input(36) == True:
-                print ('Back')
-                s.sendall(b'Back')
+        elif GPIO.input(32) == False and GPIO.input(36) == True and GPIO.input(38) == True:
+            print ('Back')
+            s.sendall(b'Back')
 
-        elif GPIO.input(38) == True and GPIO.input(35) == True and GPIO.input(33) == False:
-                print ('Right')
-                s.sendall(b'Right')
+        elif GPIO.input(40) == True and GPIO.input(32) == True and GPIO.input(36) == False:
+            print ('Right')
+            s.sendall(b'Right')
 
-        elif GPIO.input(38) == True and GPIO.input(35) == True and GPIO.input(36) == False:
-                print ('Left')
-                s.sendall(b'Left')
+        elif GPIO.input(40) == True and GPIO.input(32) == True and GPIO.input(38) == False:
+            print ('Left')
+            s.sendall(b'Left')
 
-        elif GPIO.input(38) == False and GPIO.input(36) == False:
-                print ('Forward and Left')
-                s.sendall(b'Forward and Left')
+        elif GPIO.input(40) == False and GPIO.input(38) == False:
+            print ('Forward and Left')
+            s.sendall(b'Forward and Left')
 
-        elif GPIO.input(38) == False and GPIO.input(33) == False:
-                print ('Forward and Right')
-                s.sendall(b'Forward and Right')
+        elif GPIO.input(40) == False and GPIO.input(36) == False:
+            print ('Forward and Right')
+            s.sendall(b'Forward and Right')
 
-        elif GPIO.input(35) == False and GPIO.input(36) == False:
-                print ('Back and Left')
-                s.sendall(b'Back and Left')
+        elif GPIO.input(32) == False and GPIO.input(38) == False:
+            print ('Back and Left')
+            s.sendall(b'Back and Left')
 
-        elif GPIO.input(35) == False and GPIO.input(33) == False:
-                print ('Back and Right')
-                s.sendall(b'Back and Right')
-        elif GPIO.input(38) == True and GPIO.input(35) == True and GPIO.input(36) == True and GPIO.input(33) == True:
-                print (' ')
-                s.sendall(b' ')
+        elif GPIO.input(32) == False and GPIO.input(36) == False:
+            print ('Back and Right')
+            s.sendall(b'Back and Right')
+        
+        elif GPIO.input(40) == True and GPIO.input(32) == True and GPIO.input(38) == True and GPIO.input(36) == True:
+            print ('stop')
+            s.sendall(json.dumps({'lin': 0, 'ang': 0}).decode())
+        
+def four_Way(socket_object):
+	while status['running'] == True:
+		time.sleep(0.1)
+		if GPIO.input(40) == False and GPIO.input(36) == True and GPIO.input(38) == True:
+			print ('Forward')
+			s.sendall(b'Forward')
 
-def fourWay(s):
-    while True:
+		elif GPIO.input(32) == False and GPIO.input(36) == True and GPIO.input(38) == True:
+			print ('Back')
+			s.sendall(b'Back')
+
+		elif GPIO.input(40) == True and GPIO.input(32) == True and GPIO.input(36) == False:
+			print ('Right')
+			s.sendall(b'Right')
+
+		elif GPIO.input(40) == True and GPIO.input(32) == True and GPIO.input(38) == False:
+			print ('Left')
+			s.sendall(b'Left')
+        
+		elif GPIO.input(40) == True and GPIO.input(32) == True and GPIO.input(38) == True and GPIO.input(36) == True:
+			print ('stop')
+			s.sendall(json.dumps({'lin': 0, 'ang': 0}).decode())
+			break #Returns control to drive()
+        
+def two_Way(socket_object):
+    while status['running'] == True:
         time.sleep(0.1)
-        if GPIO.input(38) == False and GPIO.input(33) == True and GPIO.input(36) == True:
-                print ('Forward')
-                s.sendall(b'Forward')
+        if GPIO.input(40) == False and GPIO.input(36) == True and GPIO.input(38) == True:
+            print ('Forward')
+            s.sendall(b'Forward')
 
-        elif GPIO.input(35) == False and GPIO.input(33) == True and GPIO.input(36) == True:
-                print ('Back')
-                s.sendall(b'Back')
-
-        elif GPIO.input(38) == True and GPIO.input(35) == True and GPIO.input(33) == False:
-                print ('Right')
-                s.sendall(b'Right')
-
-        elif GPIO.input(38) == True and GPIO.input(35) == True and GPIO.input(36) == False:
-                print ('Left')
-                s.sendall(b'Left')
-
-        elif GPIO.input(38) == True and GPIO.input(35) == True and GPIO.input(36) == True and GPIO.input(33) == True:
-                print (' ')
-                s.sendall(b' ')
-
-def twoWay(s):
-    while True:
+        elif GPIO.input(32) == False and GPIO.input(36) == True and GPIO.input(38) == True:
+            print ('Back')
+            s.sendall(b'Back')
+        
+        elif GPIO.input(40) == True and GPIO.input(32) == True and GPIO.input(38) == True and GPIO.input(36) == True:
+                print ('stop')
+                s.sendall(json.dumps({'lin': 0, 'ang': 0}).decode())
+        
+def angular(socket_object):
+    while status['running'] == True:
         time.sleep(0.1)
-        if GPIO.input(38) == False and GPIO.input(33) == True and GPIO.input(36) == True:
-                print ('Forward')
-                s.sendall(b'Forward')
+        if GPIO.input(40) == False and GPIO.input(38) == False:
+            print ('Forward and Left')
+            s.sendall(b'Forward and Left')
 
-        elif GPIO.input(35) == False and GPIO.input(33) == True and GPIO.input(36) == True:
-                print ('Back')
-                s.sendall(b'Back')
+        elif GPIO.input(40) == False and GPIO.input(36) == False:
+            print ('Forward and Right')
+            s.sendall(b'Forward and Right')
 
-        elif GPIO.input(38) == True and GPIO.input(35) == True and GPIO.input(36) == True and GPIO.input(33) == True:
-                print (' ')
-                s.sendall(b' ')
+        elif GPIO.input(32) == False and GPIO.input(38) == False:
+            print ('Back and Left')
+            s.sendall(b'Back and Left')
 
-def angularWay(s):
-    while True:
-        time.sleep(0.1)
-        if GPIO.input(38) == False and GPIO.input(36) == False:
-                print ('Forward and Left')
-                s.sendall(b'Forward and Left')
+        elif GPIO.input(32) == False and GPIO.input(36) == False:
+            print ('Back and Right')
+            s.sendall(b'Back and Right')
+        
+        elif GPIO.input(40) == True and GPIO.input(32) == True and GPIO.input(38) == True and GPIO.input(36) == True:
+                print ('stop')
+                s.sendall(json.dumps({'lin': 0, 'ang': 0}).decode())
+        
 
-        elif GPIO.input(38) == False and GPIO.input(33) == False:
-                print ('Forward and Right')
-                s.sendall(b'Forward and Right')
+def send_dict(socket_object, info):
+	"""Sends dictionary with relevant info to turtlebot"""
+	socket_object.sendall(json.dumps(info).decode())
 
-        elif GPIO.input(35) == False and GPIO.input(36) == False:
-                print ('Back and Left')
-                s.sendall(b'Back and Left')
-
-        elif GPIO.input(35) == False and GPIO.input(33) == False:
-                print ('Back and Right')
-                s.sendall(b'Back and Right')
-        elif GPIO.input(38) == True and GPIO.input(35) == True and GPIO.input(36) == True and GPIO.input(33) == True:
-                print (' ')
-                s.sendall(b' ')
-
+def drive(control_mode):
+    while status['running']==True: 
+        if control_mode == 'four_way':
+            four_Way(s)
+        elif control_mode == 'eight_way':
+                eight_Way(s)
+        elif control_mode == 'two_way':
+            two_Way(s)
+        elif control_mode == 'angular':
+            angular(s)
+		else:
+			wait_for_input(control_mode)
+#Possibly redundant - currently trying out difficulty handling from the class
+def set_difficulty(settings):
+    global player
+    player = playerClass.Player(settings)
 
--- controller/playerClass.py
+++ controller/playerClass.py
@@ -1,31 +1,53 @@
-import socket
-
 class Player():
-    
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    def __init__(self, name = 'player', speed = 1, control_mode = 'fourway'):
+    def __init__(self, name = 'player', lin_scale = 1, ang_scale = 1, control_mode = 'four_way', robot = '192.168.1.38', image = ""):
+        self.max_lin_speed = 0.22
+        self.max_ang_speed = 2.84
         self.name = name
-        self.speed = speed
+        #self.speeds = {}
+        self.lin_speed = lin_scale * self.max_lin_speed
+        self.ang_speed = ang_scale * self.max_ang_speed
         self.control_mode = control_mode
+        self.robot = robot
+        self.image = image
 
-    def choose_bot38(self):
-        self.robot_choice = '192.168.1.38'
-    def choose_bot39(self):
-        self.robot_choice = '192.168.1.38'
-
-    def Connect(self, HOST, PORT, socket_object):
-        """Connects to the desired turtlebot corresponding to the ip-address
-        passed to it"""
-        try:
-            socket_object.connect((HOST, PORT))
-        except:
-            print("Couldn't connect to TurtleBot with address " + HOST)
-            
-    def drive(self):
-        pass
+    def set_angular(self, ang_lin=0.8, ang_ang=0.2):
+        self.ang_lin = ang_lin * self.lin_speed #New linear speed for curved movement
+        self.ang_ang = ang_ang * self.ang_speed #New angular speed for curved movement    
+    def easy(self):
+        self.lin_speed = 1.0 * self.max_lin_speed
+        self.ang_speed = 0.25 * self.max_ang_speed
+        self.control_mode = 'eight_way'
+        self.set_angular()
+    def medium(self):
+        self.lin_speed = 0.8 * self.max_lin_speed
+        self.ang_speed = 0.5 * self.max_ang_speed
+        self.control_mode = 'four_way'
+        #self.set_angular() 
+    def hard(self):
+        self.lin_speed = 0.6 * self.max_lin_speed
+        self.ang_speed = 0.75 * self.max_ang_speed
+        self.control_mode = 'four_way'
+        #self.set_angular()
+    def very_hard(self):
+        self.lin_speed = 0.4 * self.max_lin_speed
+        self.ang_speed = 1.0 * self.max_ang_speed
+        self.control_mode = 'angular'
+        self.set_angular()
+    def new_difficulty(self, difficulty_params):
+        """Function intended to create a new difficulty based on
+        difficulty parameters from a dictionary"""
+        self.lin_speed = difficulty_params['lin_scale'] * self.max_lin_speed
+        self.ang_speed = difficulty_params['ang_scale'] * self.max_ang_speed
+        self.control_mode = difficulty_params['control_mode']
+        self.set_angular()
+    
+    
 
 if __name__ == '__main__':
-    kwargs = {"name": "martin", "speed": 0.5}
+    kwargs = {"name": "martin", "lin_scale": 0.5}
     player1 = Player(**kwargs)
-    print(player1.name, player1.control_mode)
+    print(player1.lin_speed, player1.ang_speed, player1.control_mode)
+    player1.medium()
+    print(player1.lin_speed, player1.ang_speed, player1.control_mode)
+
+
