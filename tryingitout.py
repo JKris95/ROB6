c084e3c0f4cb83676ac0703b4a7298ccffb38bbe
JKris95 <jk03071995@gmail.com>
2018-04-19 15:28:04 +0200

    Argument for thread creation should now be considered a tuple

--- tryingitout.py
+++ tryingitout.py
@@ -10,5 +10,12 @@ def test():
 test()
 objectScope.category = 'animals'
 """
-a = dict([('hej', 3), ('farvel', 4)])
-print(a)
+#a = dict([('hej', 3), ('farvel', 4)])
+#print(a)
+
+
+
+def print_tuple(*args=()):
+    print(args)
+
+print_tuple('fourway')
