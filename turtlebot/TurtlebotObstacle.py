f5506fbc854d0adac87382fb7ef86e8cc4bf98f4
CasperMariager <cs.mariager@gmail.com>
2018-05-02 15:38:05 +0200

    Changed the way object detection works

---
+++ turtlebot/TurtlebotObstacle.py
@@ -0,0 +1,66 @@
+
+#!/usr/bin/env python
+#################################################################################
+# Copyright 2018 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#################################################################################
+
+# Authors: Gilbert #
+
+import rospy
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+
+
+class Obstacle():
+    def __init__(self):
+        self.pub = rospy.Publisher('detect_angle', String, queue_size=10)
+        self.LIDAR_ERR = 0.05
+
+    def get_scan(self, data, min_val, max_val):
+        scan = LaserScan()
+        scan = data
+        for i in range(360):
+            if i <= min_val or i > max_val:
+                if scan.ranges[i] >= self.LIDAR_ERR:
+                    self.scan_filter.append(scan.ranges[i])
+
+    def obstacle(self, steps, start, distance, direction):
+        while not rospy.is_shutdown():
+            msg = rospy.wait_for_message("/scan", LaserScan)
+            self.scan_filter = []
+            msg_ranges_lenght = len(msg.ranges)
+            for i in range(steps):
+                if msg.ranges[start] >= self.LIDAR_ERR:
+                    self.scan_filter.append(msg.ranges[start])
+                    start = (start + 1) % msg_ranges_lenght
+
+            if min(self.scan_filter) < distance:
+                self.pub.publish(direction)
+                print(direction)
+
+            else:
+                self.pub.publish('Nothing')
+                print('Nothing')
+
+
+def main():
+    rospy.init_node('turtlebot3_obstacle')
+    try:
+        obstacle = Obstacle()
+    except rospy.ROSInterruptException:
+        pass
+
+if __name__ == '__main__':
+    main()
--- turtlebot/obj_detect.py
+++ turtlebot/obj_detect.py
@@ -1,50 +1,13 @@
-import rospy
-from sensor_msgs.msg import LaserScan
-from std_msgs.msg import String
+from TurtlebotObstacle import Obstacle
 
-rospy.init_node('turtlebot3_obstacle', anonymous=True)
-pub = rospy.Publisher('/det_ang', String, queue_size=10)
+Obstacles = Obstacle()
 
-def obstacle():
-    someVal = 0
-    lidarErr = 0.05 #Some error values
-    while True:
-        msg = rospy.wait_for_message("/scan", LaserScan)
-        scan_filter = []
-        scan_filter2 = []
-        for i in range(360):
-            if i <= 50 or i >= 300: #Front values
-                if msg.ranges[i] >= lidarErr:
-                    scan_filter.append(msg.ranges[i])
-                    if min(scan_filter) < 0.1: #Range where the turtlebot will react
-                        pub.publish('Front')
-                        #rospy.loginfo('Front')
-                        someVal = 200
-                    elif someVal is not 0: #Not a pretty way to only publish one time, but i works with little to no delay.
-                        someVal = someVal - 1
-                        if someVal == 0:
-                            pub.publish('Nothing')
-
-
-            elif i <= 230 and i >= 130: #Back values
-                if msg.ranges[i] >= lidarErr:
-                    scan_filter2.append(msg.ranges[i])
-                    if min(scan_filter2) < 0.1: #Range where the turtlebot will will react
-                        pub.publish('Back')
-                        #rospy.loginfo('Back')
-                        someVal = 200
-                    elif someVal is not 0:
-                        someVal = someVal - 1
-                        if someVal == 0: 
-                            pub.publish('Nothing')
-
-
-obstacle()
+while True:
+    Obstacles.obstacle(100, 300, 0.2, 'Front')
+    Obstacles.obstacle(100, 130, 0.2, 'Back')
 
 
 
 
 
-
-
-
+     
