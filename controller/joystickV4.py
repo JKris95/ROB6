412234d4886a16cb32b4d57a01b9b0614c7413b0
Daniel Fischer <robotics.aau.daniel@gmail.com>
2018-05-04 18:44:03 +0200

    full screeen

--- controller/joystickV4.py
+++ controller/joystickV4.py
@@ -95,7 +95,7 @@ class GUI_player_screen(GUI_base):
 
 def main(): 
 	root = tk.Tk()
-	#root.attributes('-fullscreen',True)
+	root.attributes('-fullscreen',True)
 	GUI_select_robot(root)
 	root.mainloop()
 
