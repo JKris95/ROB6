4c3e7f16bfa31d18ad500653650d69bfeb6070f0
CasperMariager <cs.mariager@gmail.com>
2018-05-04 13:31:55 -0100

    Fixed the object detection

--- turtlebot/TurtlebotObstacle.py
+++ turtlebot/TurtlebotObstacle.py
@@ -22,13 +22,14 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
-
+import numpy as np
 
 
 class Obstacle():
     def __init__(self):
-        self.pub = rospy.Publisher('detect_angle', String, queue_size=10)
+        self.pub = rospy.Publisher('det_ang', String, queue_size=10)
         self.LIDAR_ERR = 0.05
+	self.is_detected = 0
 
     def get_scan(self, data, min_val, max_val):
         scan = LaserScan()
@@ -38,25 +39,50 @@ class Obstacle():
                 if scan.ranges[i] >= self.LIDAR_ERR:
                     self.scan_filter.append(scan.ranges[i])
 
+    def get_reading(self):
+	self.msg = rospy.wait_for_message("/scan", LaserScan)
+
     def obstacle(self, steps, start, distance, direction):
-        msg = rospy.wait_for_message("/scan", LaserScan)
         self.scan_filter = []
-        msg_ranges_lenght = len(msg.ranges)
+        msg_ranges_lenght = len(self.msg.ranges)
         for i in range(steps):
-            if msg.ranges[start] >= self.LIDAR_ERR:
-                self.scan_filter.append(msg.ranges[start])
+            if self.msg.ranges[start] >= self.LIDAR_ERR and self.msg.ranges[start] <= distance:
+                self.scan_filter.append(self.msg.ranges[start])
             start = (start + 1) % msg_ranges_lenght
-	    print(start)
+	    #print(start)
 
-        if self.scan_filter and min(self.scan_filter) < distance:
-            #self.pub.publish(direction)
-	    #print(self.scan_filter)
+	if np.mean(self.scan_filter) >= 0.18 and len(self.scan_filter) >= 10:
+            self.pub.publish(direction)
+	    print(direction)
+	    self.is_detected = 1
+
+        elif np.mean(self.scan_filter) >= 0.195 and len(self.scan_filter) >= 9:
+            self.pub.publish(direction)
             print(direction)
+            self.is_detected = 1
 
-        else:
+        elif np.mean(self.scan_filter) >= 0.222 and len(self.scan_filter) >= 8:
+            self.pub.publish(direction)
+            print(direction)
+            self.is_detected = 1
+
+        elif np.mean(self.scan_filter) >= 0.271 and len(self.scan_filter) >= 6:
+            self.pub.publish(direction)
+            print(direction)
+            self.is_detected = 1
+
+        elif np.mean(self.scan_filter) >= 0.291 and len(self.scan_filter) >= 3:
+            self.pub.publish(direction)
+            print(direction)
+            self.is_detected = 1
+
+
+        elif self.is_detected == 0:
             self.pub.publish('Nothing')
             #print('Nothing')
 
+	else:
+	    self.is_detected = 0
 
 def main():
     #rospy.init_node('turtlebot3_obstacle')
--- turtlebot/obj_detect.py
+++ turtlebot/obj_detect.py
@@ -8,8 +8,9 @@ Obstacles = Obstacle()
 
 while True:
     #start = time.time()
-    Obstacles.obstacle(100, 300, 0.2, 'Front')
-    #Obstacles.obstacle(100, 130, 0.2, 'Back')
+    Obstacles.get_reading()
+    Obstacles.obstacle(100, 300, 0.3, 'Front')
+    Obstacles.obstacle(100, 130, 0.3, 'Back')
     #end = time.time()
     #print(end-start)
 
