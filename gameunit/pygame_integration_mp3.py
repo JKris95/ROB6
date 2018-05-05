562dd01fad80fd0093b37f4e43c9e610f1240530
Daniel Fischer <robotics.aau.daniel@gmail.com>
2018-05-05 20:49:32 +0200

    pygame integration

---
+++ gameunit/pygame_integration_mp3.py
@@ -0,0 +1,22 @@
+import pygame
+import time
+
+
+#Enabling pygame to play sounds
+pygame.mixer.init()
+
+
+def start_countdowm_music(length_countdown):
+    start_pos = 20 - length_countdown
+    
+     #TODO: add while loop with the boolean variable here that tells when the countdown stops. 
+    #while play_okay == True:
+    path = 'countdown.mp3'  #ændre path alt efter hvor scriptet kører fra, denne virker hvis det køres fra /gameunit.
+    pygame.mixer.music.load(path)
+    pygame.mixer.music.play(0,start_pos)
+    time.sleep(length_countdown)
+
+
+    pygame.mixer.music.stop() #this is after the while loop, if the while loop breaks from change in bool value, 
+
+start_countdowm_music(7)
