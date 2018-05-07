3fa8f1d363a920561914a8c05b8b8b445003a2b7
Daniel Fischer <robotics.aau.daniel@gmail.com>
2018-05-07 13:39:57 +0200

    tabs and spaces

--- turtlebot/TurtlebotObstacleClass.py
+++ turtlebot/TurtlebotObstacleClass.py
@@ -26,70 +26,70 @@ import numpy as np
 
 
 class Obstacle():
-    def __init__(self):
-        self.pub = rospy.Publisher('det_ang', String, queue_size=10)
-        self.LIDAR_ERR = 0.05
-	self.is_detected = 0
+	def __init__(self):
+		self.pub = rospy.Publisher('det_ang', String, queue_size=10)
+		self.LIDAR_ERR = 0.05
+		self.is_detected = 0
 
-    def get_scan(self, data, min_val, max_val):
-        scan = LaserScan()
-        scan = data
-        for i in range(360):
-            if i <= min_val or i > max_val:
-                if scan.ranges[i] >= self.LIDAR_ERR:
-                    self.scan_filter.append(scan.ranges[i])
+	def get_scan(self, data, min_val, max_val):
+		scan = LaserScan()
+		scan = data
+		for i in range(360):
+			if i <= min_val or i > max_val:
+				if scan.ranges[i] >= self.LIDAR_ERR:
+					self.scan_filter.append(scan.ranges[i])
 
-    def get_reading(self):
-	self.msg = rospy.wait_for_message("/scan", LaserScan)
+	def get_reading(self):
+		self.msg = rospy.wait_for_message("/scan", LaserScan)
 
-    def obstacle(self, steps, start, distance, direction):
-        self.scan_filter = []
-        msg_ranges_lenght = len(self.msg.ranges)
-        for i in range(steps):
-            if self.msg.ranges[start] >= self.LIDAR_ERR and self.msg.ranges[start] <= distance:
-                self.scan_filter.append(self.msg.ranges[start])
-            start = (start + 1) % msg_ranges_lenght
-	    #print(start)
+	def obstacle(self, steps, start, distance, direction):
+		self.scan_filter = []
+		msg_ranges_lenght = len(self.msg.ranges)
+		for i in range(steps):
+			if self.msg.ranges[start] >= self.LIDAR_ERR and self.msg.ranges[start] <= distance:
+				self.scan_filter.append(self.msg.ranges[start])
+			start = (start + 1) % msg_ranges_lenght
+		#print(start)
 
-	if np.mean(self.scan_filter) >= 0.18 and len(self.scan_filter) >= 10:
-            self.pub.publish(direction)
-	    print(direction)
-	    self.is_detected = 1
+		if np.mean(self.scan_filter) >= 0.18 and len(self.scan_filter) >= 10:
+			self.pub.publish(direction)
+			print(direction)
+			self.is_detected = 1
 
-        elif np.mean(self.scan_filter) >= 0.195 and len(self.scan_filter) >= 9:
-            self.pub.publish(direction)
-            print(direction)
-            self.is_detected = 1
+		elif np.mean(self.scan_filter) >= 0.195 and len(self.scan_filter) >= 9:
+			self.pub.publish(direction)
+			print(direction)
+			self.is_detected = 1
 
-        elif np.mean(self.scan_filter) >= 0.222 and len(self.scan_filter) >= 8:
-            self.pub.publish(direction)
-            print(direction)
-            self.is_detected = 1
+		elif np.mean(self.scan_filter) >= 0.222 and len(self.scan_filter) >= 8:
+			self.pub.publish(direction)
+			print(direction)
+			self.is_detected = 1
 
-        elif np.mean(self.scan_filter) >= 0.271 and len(self.scan_filter) >= 6:
-            self.pub.publish(direction)
-            print(direction)
-            self.is_detected = 1
+		elif np.mean(self.scan_filter) >= 0.271 and len(self.scan_filter) >= 6:
+			self.pub.publish(direction)
+			print(direction)
+			self.is_detected = 1
 
-        elif np.mean(self.scan_filter) >= 0.291 and len(self.scan_filter) >= 3:
-            self.pub.publish(direction)
-            print(direction)
-            self.is_detected = 1
+		elif np.mean(self.scan_filter) >= 0.291 and len(self.scan_filter) >= 3:
+			self.pub.publish(direction)
+			print(direction)
+			self.is_detected = 1
 
 
-        elif self.is_detected == 0:
-            self.pub.publish('Nothing')
-            #print('Nothing')
+		elif self.is_detected == 0:
+			self.pub.publish('Nothing')
+			#print('Nothing')
 
-	else:
-	    self.is_detected = 0
+		else:
+			self.is_detected = 0
 
 def main():
-    #rospy.init_node('turtlebot3_obstacle')
-    try:
-        obstacle = Obstacle()
-    except rospy.ROSInterruptException:
-        pass
+	#rospy.init_node('turtlebot3_obstacle')
+	try:
+		obstacle = Obstacle()
+	except rospy.ROSInterruptException:
+		pass
 
 if __name__ == '__main__':
-    main()
+	main()
