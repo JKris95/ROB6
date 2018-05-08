6830e76363af376dd229e5d9f5a52f12a65037b4
CasperMariager <cs.mariager@gmail.com>
2018-05-08 10:25:06 +0200

    We will not reverse away from objects when driving back

--- turtlebot/obj_detect_node.py
+++ turtlebot/obj_detect_node.py
@@ -3,17 +3,22 @@ import rospy
 import time
 
 rospy.init_node('turtlebot3_obstacle', anonymous=True)
+rospy.Subscriber("/state", String, state)
 
 Obstacles = Obstacle()
 
 while True:
     #start = time.time()
     Obstacles.get_reading()
-    Obstacles.obstacle(100, 300, 0.3, 'Front')
-    Obstacles.obstacle(100, 130, 0.3, 'Back')
+    if state == 'Nothing' or 'Front' or 'Back':
+        Obstacles.obstacle(100, 300, 0.3, 'Front')
+        Obstacles.obstacle(100, 130, 0.3, 'Back')
     #end = time.time()
     #print(end-start)
 
+def state(data): #Subscriber which listen to topic state
+	global state 
+	state = data.data
 
 
 
