6fa67eb2bdc14ab29b351b86a564cd82ea7abdd8
CasperMariager <cs.mariager@gmail.com>
2018-05-16 13:45:57 +0200

    bug fix

--- turtlebot/connection_node.py
+++ turtlebot/connection_node.py
@@ -3,10 +3,18 @@ import time
 import socket
 from std_msgs.msg import String
 
+
 HOST = '192.168.1.34'  
 PORT = 50007
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        
-s.connect((HOST, PORT))
+
+while True:
+    try:
+        s.connect((HOST, PORT))
+        break
+    except:
+        print("did not connect")
 
 rospy.Subscriber("/turtlebot_state", String, turtlebot_state_function)
 pub = rospy.Publisher('turtlebot_state', String, queue_size=10)
