ab371012d426ca655ff27d8f1b7f19e6e8d9a96d
Daniel Fischer <robotics.aau.daniel@gmail.com>
2018-05-17 13:44:07 +0200

    left_hand

--- controller/joystickV4.py
+++ controller/joystickV4.py
@@ -50,10 +50,10 @@ class GUI_select_difficulty(GUI_base):
 	def __init__(self, master):
 		GUI_base.__init__(self,master)
 		#self.quitButton = tk.Button(self.frame, text = 'Back', width = 25, height = 5, command = lambda *args:[self.close_window(GUI_select_robot)])
-		self.difficulty_1 = tk.Button(self.frame, text = 'Difficulty 1', width = 25, height = 5, command = lambda *args:[player.easy(),self.unpacker(self.window_list), self.new_window(GUI_select_player)])
-		self.difficulty_2 = tk.Button(self.frame, text = 'Difficulty 2', width = 25, height = 5, command = lambda *args:[player.medium(),self.unpacker(self.window_list), self.new_window(GUI_select_player)])
-		self.difficulty_3 = tk.Button(self.frame, text = 'Difficulty 3', width = 25, height = 5, command = lambda *args:[player.hard(),self.unpacker(self.window_list), self.new_window(GUI_select_player)])
-		self.difficulty_4 = tk.Button(self.frame, text = 'Difficulty 4', width = 25, height = 5, command = lambda *args:[player.very_hard(),self.unpacker(self.window_list), self.new_window(GUI_select_player)])
+		self.difficulty_1 = tk.Button(self.frame, text = 'Mode 1', width = 25, height = 5, command = lambda *args:[player.easy(),self.unpacker(self.window_list), self.new_window(GUI_select_player)])
+		self.difficulty_2 = tk.Button(self.frame, text = 'Mode 2', width = 25, height = 5, command = lambda *args:[player.medium(),self.unpacker(self.window_list), self.new_window(GUI_select_player)])
+		self.difficulty_3 = tk.Button(self.frame, text = 'Mode 3', width = 25, height = 5, command = lambda *args:[player.hard(),self.unpacker(self.window_list), self.new_window(GUI_select_player)])
+		self.difficulty_4 = tk.Button(self.frame, text = 'Mode 4', width = 25, height = 5, command = lambda *args:[player.very_hard(),self.unpacker(self.window_list), self.new_window(GUI_select_player)])
 		self.cb_var = tk.BooleanVar()
 		self.flip = tk.Checkbutton(self.frame, text = 'Flip', width = 25, variable = self.cb_var, onvalue = True, offvalue = False, command=self.change_flip_state)
 		self.append_window_list( self.frame, self.difficulty_1, self.difficulty_2, self.difficulty_3, self.difficulty_4, self.flip)
@@ -74,7 +74,7 @@ class GUI_select_player(GUI_base):
 		self.player_natasja = tk.Button(self.frame, text = 'Natasja', width = 25, height = 5,  command = lambda *args:[player.change_settings(player.player_info, ['name'], ['Natasja']), change_dict_pair(status, 'running', True), send_dict(gameunit_conn, dict([ ('name', player.player_info['name']), ('robot', player.player_info['robot']) ])), self.unpacker(self.window_list), self.new_window(GUI_player_screen)])
 		self.player_guest = tk.Button(self.frame, text = 'Gæst', width = 25, height = 5, command = lambda *args:[player.change_settings(player.player_info, ['name'], ['Gæst']), change_dict_pair(status, 'running', True), send_dict(gameunit_conn, dict([ ('name', player.player_info['name']), ('robot', player.player_info['robot']) ])), self.unpacker(self.window_list), self.new_window(GUI_player_screen)])
 		self.reverse_var = tk.BooleanVar()
-		self.reverse_directions = tk.Checkbutton(self.frame, text = 'Reverse', width = 25, variable = self.reverse_var, onvalue = True, offvalue = False, command=player.reverse_directions)
+		self.reverse_directions = tk.Checkbutton(self.frame, text = 'Left hand', width = 25, variable = self.reverse_var, onvalue = True, offvalue = False, command=player.reverse_directions)
 		self.append_window_list(self.quitButton, self.frame, self.player_martin, self.player_nina, self.player_natasja, self.player_guest, self.reverse_directions)
 		self.packer(self.window_list)
 
