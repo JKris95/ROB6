b8c9cb3a0dc390a17b4a122c502c6f0ae94f4636
CasperMariager <cs.mariager@gmail.com>
2018-06-02 15:42:33 +0200

    big fix

--- turtlebot/connection_node.py
+++ turtlebot/connection_node.py
@@ -33,7 +33,7 @@ while True:
 	recv_data = s.recv(1024)
 	if recv_data == 'go back':
 		time.sleep(0.5)
-		for i in range(0,5)
+		for i in range(0,5):
 			pub.publish(recv_data)
 			time.sleep(0.1)
 		print('go back')
